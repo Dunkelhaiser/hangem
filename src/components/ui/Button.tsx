8cc661215f576c3f9560f59328ff5226385c85db
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-12-25 19:00:15 +0200

    fix: temporary use inline variable for shadows

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,19 +2,22 @@ import { Button as ButtonPrimitive } from "@base-ui/react/button";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const pressableButtonShadow = `hover:translate-hover hover:shadow-medium
-        focus-visible:translate-hover focus-visible:shadow-medium
-        active:translate-active active:shadow-base active:duration-75`;
+// TODO: fix to use custom Tailwind variables for shadows
+const pressableButtonShadow = `shadow-[3.5px_3.5px_0px]
+        hover:translate-hover hover:shadow-[2.75px_2.75px_0px]
+        focus-visible:translate-hover focus-visible:shadow-[2.75px_2.75px_0px]
+        active:translate-active active:shadow-[1.5px_1.5px_0px] active:duration-75
+    `;
 
 const buttonVariants = cva(
     "relative inline-flex items-center justify-center whitespace-nowrap font-mono font-black uppercase tracking-widest transition-all outline-none select-none cursor-pointer disabled:opacity-50 disabled:pointer-events-none active:duration-75 border-2 rounded-sketchy",
     {
         variants: {
             variant: {
-                default: `bg-primary text-primary-foreground border-border shadow-ring shadow-deep ${pressableButtonShadow}`,
-                secondary: `bg-secondary text-secondary-foreground border-ring shadow-ring shadow-deep ${pressableButtonShadow}`,
+                default: `bg-primary text-primary-foreground border-border shadow-ring ${pressableButtonShadow}`,
+                secondary: `bg-secondary text-secondary-foreground border-ring shadow-ring ${pressableButtonShadow}`,
                 outline: `
-                    border-border text-border shadow-ring shadow-deep
+                    border-border text-border shadow-ring
                     hover:bg-secondary/5 focus-visible:bg-secondary/5 active:bg-secondary/5
                     ${pressableButtonShadow}
                     dark:text-secondary-foreground
@@ -27,7 +30,7 @@ const buttonVariants = cva(
                     dark:text-secondary-foreground/80 dark:hover:bg-secondary/30 dark:active:bg-secondary/30 dark:focus-visible:bg-secondary/30
                 `,
                 destructive: `
-                    bg-destructive/5 text-destructive border-destructive shadow-destructive shadow-deep
+                    bg-destructive/5 text-destructive border-destructive shadow-destructive
                     hover:bg-destructive/10 focus-visible:bg-destructive/10 active:bg-destructive/10
                     ${pressableButtonShadow}
                     dark:bg-destructive/15 dark:hover:bg-destructive/20 dark:active:bg-destructive/20
