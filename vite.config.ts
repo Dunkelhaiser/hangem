cf4eed6c4423ce78a89cdec29baabb6a8237667b
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2026-01-05 21:13:15 +0200

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -19,6 +20,7 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
+        VitePWA({ registerType: "autoUpdate", workbox: { maximumFileSizeToCacheInBytes: 9_000_000 } }),
     ],
     optimizeDeps: {
         exclude: ["@electric-sql/pglite"],
